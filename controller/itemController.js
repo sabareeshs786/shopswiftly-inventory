ea186ac8620c841f362a14d8f27421b3095ec45e
Hackovator <ssabareesh646@gmail.com>
2023-12-16 14:33:59 +0530

    Update itemController.js

--- controller/itemController.js
+++ controller/itemController.js
@@ -1,18 +1,25 @@
 const Mobile = require('../models/mobiles');
 
 const getItems = async (req, res) => {
-    const category = req.params.category;
-    const preview = Boolean(req.query.preview);
-    const brand = req.query.brand;
-
+    const category = req.params.category || "mobiles";
+    const preview = req.query.preview ? 
+    String(req.query.preview).toLowerCase() == "true" 
+    ? true : String(req.query.preview).toLowerCase() == "false" ?
+    false : true
+    : true;
+    const brand = req.query.brand?.split(',') || "apple";
+    const sort = req.query.sort || "price";
+    const sortOrder = {}
+    sortOrder[sort] = 1;
+    console.log(preview);
     if (category == "mobiles") {
         try {
             if (preview) {
-                const items = await Mobile.find({brand: brand}).limit(5);
+                const items = await Mobile.find({brand: {$in: brand }}).limit(5).sort(sortOrder);
                 if (!items) return res.status(204).json({ 'message': `No items found for the category ${category}` });
                 res.json(items);
             } else {
-                const items = await Mobile.find({brand: brand}).limit(20);
+                const items = await Mobile.find({brand: {$in: brand }}).limit(20).sort(sortOrder);
                 if (!items) return res.status(204).json({ 'message': `No items found for the category ${category}` });
                 res.json(items);
             }
